05e0d3544d49951b7373e92bf09925837f667baf
Willem <willemsporen@gmail.com>
2024-12-27 12:14:12 +0100

    Initial commit

---
+++ Net9CompilerCrash/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+IList<string> dataSource = ["1", "2"];
+
+Filter(dataSource.AsQueryable(), new()
+{
+    {
+        "testColumn",
+        (q, str) => q.Where(x => x == $"{str}")
+    }
+});
+    
+void Filter<T>(IQueryable<T> queryable, Dictionary<string, Expression<Func<IQueryable<T>, string[], IQueryable<T>>>> filterMembers)
+{
+    // Implementation not important
+}
